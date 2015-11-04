b41e23ad6e015ac3416d4950dcf761848bb47adf
msisti <michelle.sisti@gmail.com>
2015-11-04 01:45:04 +0000

    generated scaffold for likes

--- ej/config/routes.rb
+++ ej/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :likes
   resources :notes
   resources :posts
   resources :comments
--- ej/db/schema.rb
+++ ej/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104002729) do
+ActiveRecord::Schema.define(version: 20151104013405) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20151104002729) do
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notes", force: :cascade do |t|
