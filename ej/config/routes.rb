e4d476f560a1fb29d00cd603a88aa293c1ca2b74
msisti <michelle.sisti@gmail.com>
2015-11-04 13:59:55 +0000

    added text editors to form views

--- ej/config/routes.rb
+++ ej/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   resources :likes
   resources :notes
   resources :posts
--- ej/db/schema.rb
+++ ej/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104013405) do
+ActiveRecord::Schema.define(version: 20151104133021) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -20,6 +20,22 @@ ActiveRecord::Schema.define(version: 20151104013405) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",    limit: 255, null: false
+    t.string   "data_content_type", limit: 255
+    t.integer  "data_file_size",    limit: 4
+    t.integer  "assetable_id",      limit: 4
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width",             limit: 4
+    t.integer  "height",            limit: 4
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.text     "content",    limit: 65535
