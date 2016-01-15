b306500e7feee5655b27dc91a7168c540e1831fe
kenharsch <kharsch@brandeis.edu>
2016-01-14 20:06:18 -0500

    show assignment description, add html_safe

--- ej/db/schema.rb
+++ ej/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113171108) do
+ActiveRecord::Schema.define(version: 20160114192816) do
 
   create_table "ahoy_events", force: :cascade do |t|
     t.uuid     "visit_id",   limit: 16
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20160113171108) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                         null: false
     t.datetime "updated_at",                                         null: false
+    t.boolean  "optout"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
